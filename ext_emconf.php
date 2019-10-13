fca5b38220f4f3f7b637e672a609b6ed264e141f
Georg Ringer <georg.ringer@gmail.com>
2019-10-13 16:18:11 -0400

    [TASK] Adopt description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Convert uri to links',
-    'description' => '',
+    'title' => 'Convert external urls to TYPO3 links',
+    'description' => 'Transforms url like domain.tld/services to TYPO3 links (t3://) if those are actually internal links',
     'category' => 'be',
     'author' => 'Georg Ringer',
     'author_email' => '',
     'state' => 'beta',
-    'clearCacheOnLoad' => 1,
-    'version' => '1.0.0',
+    'clearCacheOnLoad' => true,
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.2.99',
